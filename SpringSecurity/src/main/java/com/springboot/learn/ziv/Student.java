c8bfea1f3cbbbdba0679dd4761c0925cfa854a9b
Ziv <ziv@changingtec.com>
2021-08-04 23:00:43 +0800

    Add Lombok plugin.

--- SpringSecurity/src/main/java/com/springboot/learn/ziv/Student.java
+++ SpringSecurity/src/main/java/com/springboot/learn/ziv/Student.java
@@ -1,19 +1,9 @@
 package com.springboot.learn.ziv;
 
+import lombok.Data;
+
+@Data
 public class Student {
     private final Integer studentId;
     private final String studentName;
-
-    public Student(Integer studentId, String studentName) {
-        this.studentId = studentId;
-        this.studentName = studentName;
-    }
-
-    public Integer getStudentId() {
-        return studentId;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
 }
